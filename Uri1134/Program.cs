d11b70a281037d809170dbd2ff406e98da029832
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:24:01 -0300

    Add files via upload

---
+++ Uri1134/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Uri1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,alcool, gasolina, diesel;
+
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+
+            a = int.Parse(Console.ReadLine());
+            
+            while(a!=4)
+            {
+                if(a==1)
+                {
+                    alcool += +1;
+                }
+
+                if (a == 2)
+                {
+                    gasolina += +1;
+                }
+
+                if (a == 3)
+                {
+                    diesel += +1;
+                }
+
+
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Muito Obrigado");
+            Console.WriteLine();
+            Console.WriteLine("Álcool : " + alcool);
+            Console.WriteLine();
+            Console.WriteLine("Gasolina : " + gasolina);
+            Console.WriteLine();
+            Console.WriteLine("Diesel : " + diesel);
+
+
+        }
+    }
+}
